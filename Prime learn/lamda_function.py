9ea3e36309d50fb3c8bdd091710c8bc782cd8291
MayankKharbanda <kharbanda.mayank@outlook.com>
2018-08-25 12:43:08 +0530

    Update lamda_function.py

--- Prime learn/lamda_function.py
+++ Prime learn/lamda_function.py
@@ -1,3 +1,4 @@
+
 def lambda_handler(event, context):
     if event['request']['type'] == "LaunchRequest" :
         return onLaunch(event['request'], event['session'])
@@ -13,7 +14,7 @@ def onIntent(intentRequest, session):
              
     intent = intentRequest['intent']
     intentName = intentRequest['intent']['name']
-    if intentName == "whatismathfun":
+    if intentName == "whatisastrofun":
         return fun_math(intent, session)
     elif intentName == "AMAZON.HelpIntent":
         return welcomeuser()
@@ -28,9 +29,9 @@ def onSessionEnd(sessionEndedRequest, session):
 def welcomeuser():
     sessionAttributes = {}
     cardTitle = " Hello"
-    speechOutput =  "Hello , Welcome to prime learn! " \
-                    "You can know interesting facts about maths by saying Tell me math fun"
-    repromptText =  "You can know interesting facts about maths by saying Tell me math fun"
+    speechOutput =  "Hello , Welcome to astro fun! " \
+                    "You can know interesting facts about astro by saying Tell me astronomy fun"
+    repromptText =  "You can know interesting facts about astro by saying Tell me astronomy fun"
     shouldEndSession = False
     
     return buildResponse(sessionAttributes, buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession))
@@ -40,14 +41,14 @@ def fun_math(intent, session):
     index = random.randint(0,len(prime)-1)
     cardTitle = intent['name']
     sessionAttributes = {}
-    speechOutput = "Prime learn that is actually a math fact is that " + prime[index] 
-    repromptText = "You can know interesting facts about maths by saying Tell me math fun"
+    speechOutput = "Astro fact: " + prime[index] 
+    repromptText = "You can know interesting facts about astronomy by saying Tell me astronomy fun"
     shouldEndSession = True                   
     return buildResponse(sessionAttributes, buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession))
 
 def handleSessionEndRequest():
     cardTitle = "Session Ended"
-    speechOutput = "Thank you for using prime learn Alexa Skills Kit. " \
+    speechOutput = "Thank you for using astro learn Alexa Skills Kit. " \
                     "Have a great time! "
     shouldEndSession = True
     return buildResponse({}, buildSpeechletResponse(cardTitle, speechOutput, None, shouldEndSession))
@@ -84,36 +85,36 @@ def buildResponse(sessionAttr , speechlet):
 
 
 
-prime = [ "There is no largest prime number." ,
-          "The sum of the interior angles of a triangle is 180 degrees" ,
-          "There is a largest prime number in the integers modulo n",
-          "The Fibonacci sequence is encoded in the number 1/89" ,
-          "If you write out pi to two decimal places, backwards it spells pie." ,
-          "A French word for pie chart is camembert." ,
-          "The spiral shapes of sunflowers follow a Fibonacci sequence.",
-          "A pizza that has radius z and height a has volume Pi times z square multiply by a." ,
-          "The word hundred is derived from the word hundrath, which actually means 120 and not 100." ,
-          "In a room of just 23 people there’s a 50% chance that two people have the same birthday." ,
-          "Zero is the only number that can't be represented in Roman numerals.",
-          "six multiply by 9 and then add six and nine to them is equal to sixty nine." ,
-          "We tend to think of odd numbers as male and even numbers as female." ,
-          "If you shuffle a pack of cards properly, chances are that exact order has never been seen before in the whole history of the universe." ,
-          "Zero is an even number.",
+prime = [ "If the Sun were made of bananas, it would be just as hot." ,
+          "All the matter that makes up the human race could fit in a sugar cube." ,
+          "Events in the future can affect what happened in the past.",
+          "Almost all of the Universe is missing." ,
+          "Things can travel faster than light; and light doesn’t always travel very fast." ,
+          "There are an infinite number of mes writing this, and an infinite number of yous reading it." ,
+          "Black holes aren’t black.",
+          "The fundamental description of the universe does not account for a past, present or future." ,
+          "A particle here can affect one on the other side of the universe, instantaneously." ,
+          "The faster you move, the heavier you get." ,
+          "Neutron star can spin at a rate of 600 rotations per second.",
+          "If star passes too close to a black hole it can be torn apart." ,
+          "The solar system is around 4.6 billion years old. Scientists estimate that it will probably last another 5000 million years." ,
+          "If you try to learn Graham's number, your brain would change into black hole." ,
+          "Enceladus, one of Saturn's smaller moons, reflects some 90% of the sunlight, making it more reflrctive than snow.",
           "There's not enough space in the known universe to write out a googolplex on paper.",
-          "The most popular favourite number is 7.",
-          "7 also shows up a lot in human culture.",
-          "The number 4 is considered unlucky in much of Asia.",
-          "Cicadas use prime numbers as an evolutionary strategy.",
-          "10 factorial seconds is exactly 6 weeks." ,
-          "Take any four digit number, follow these steps, and you'll end up with 6174." ,
-          "555 is used by some in Thailand as slang for hahaha, because the word for five is pronounced ha." ,
-          " after a million, billion and trillion , A quadrillion, quintillion, sextillion, septillion, octillion, nonillion, decillion and undecillion come" ,
-          "Plus and Minus symbols were used as early as 1489 A.D" ,
-          "2 and 5 are the only primes that end in 2 or 5" ,
-          "An icosagon is a shape with 20 sides" ,
-          "'FOUR' is the only number in the English language that is spelt with the same number of letters as the number itself" ,
-          "Have you ever noticed that the opposite sides a die always add up to seven." ,
-          "Trigonometry is the study of the relationship between the angles of triangles and their sides",
-          "Abacus is considered the origin of the calculator" ,
-          "A 'jiffy' is an actual unit of time for 1/100th of a second"
+          "The whirlpool galaxy(M51), was the very first celestial object to be identified as being spiral.",
+          "The width of Milky Way is around 100,000 light-years.",
+          "The Sun is over 300,000 times larger than Earth.",
+          "Footprint and tire tracks left by astronauts on the Moon won't disappear as there is no wind to blow them away.",
+          "The Sun makes full rotation once every 25-35 days." ,
+          "According to mathematics, white holes are possible, although as of yet we have found none." ,
+          "The only large moon in our solar system to orbit in the opposite direction of its planet is Neptune's moon, Triton." ,
+          "There are more stars in space than grain of sand on earth, but there are more atoms in a grain then stars in universe." ,
+          "We know more about space than deep in our oceans." ,
+          "68% of universe is dark energy, and 25% is dark matter; both are invisible. This means we have seen only 5% of universe from earth." ,
+          "The Space Station circles the earth every 90 minutes." ,
+          "The first Supernovae observed outside of over own galaxy was the Andromeade, in the Andromeda galaxy in 1855." ,
+          "On Venus, it snows metal and rains sulphuric acid." ,
+          "Coca-Cola was the first drink that was ever consumed in space.",
+          "The first artificial satellite in space was called 'Sputnik'." ,
+          "Our moon is running away from earth at a rate of 4cm per year."
         ]
